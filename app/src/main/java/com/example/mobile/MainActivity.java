781f17e97fc6b068bada87086749e6d054a9a072
chaya1724 <chaya1724@gmail.com>
2019-12-18 21:56:03 +0200

    add the mobile project

---
+++ app/src/main/java/com/example/mobile/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.mobile;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Button OK;
+    Context context=this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+        OK = (Button) findViewById(R.id.login);
+        OK.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v)
+            {
+                EditText passwordField = (EditText) findViewById(R.id.editPassword);
+                EditText userNameField = (EditText) findViewById(R.id.editUserName);
+                
+                String password = passwordField.getText().toString();
+                String userName = userNameField.getText().toString();
+                
+                if(password.length()==0||userName.length()==0)
+                {
+                    AlertDialog.Builder ad = new AlertDialog.Builder(context);
+
+                    ad.setTitle( "נא למלאות את כל פרטים" + userName);
+
+                    ad.show();
+                    return;
+
+                }
+                
+            }
+
+
+
+
+        });
+    }
+}
