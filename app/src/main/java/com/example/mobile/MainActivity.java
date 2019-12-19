71878ff015394528794f6914dcd51c81a1419606
Netanel <netanel@meteo-logic.com>
2019-12-19 15:46:24 +0200

    add call url

--- app/src/main/java/com/example/mobile/MainActivity.java
+++ app/src/main/java/com/example/mobile/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.mobile;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.api.ApiDao;
 import com.model.Account;
+import com.model.User;
 
 public class MainActivity extends AppCompatActivity {
     Button OK;
@@ -16,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        Account account = new Account();
+        User account = ApiDao.launch("" , User.class  , new Pair("",""));
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
